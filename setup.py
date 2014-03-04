77aeb14af562bbeb188f08dccc1adb170ba86ae6
Duco Dokter <dokter@w20e.com>
2014-03-04 16:40:26 +0100

    release preparation

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
     ]
 
 setup(name='pu_in_core',
-      version="1.0.2",
+      version="1.0.3",
       description='PythonUnited Intranet Core',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
